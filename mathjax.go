af737b07eafa75a32850e0879be993f442cbd006
LI Tao <litao.91@bytedance.com>
2019-07-15 05:50:35 +0800

    Add Inline Math

--- mathjax.go
+++ mathjax.go
@@ -16,7 +16,11 @@ func (*mathjax) Extend(m goldmark.Markdown) {
 	m.Parser().AddOptions(parser.WithBlockParsers(
 		util.Prioritized(NewMathJaxBlockParser(), 701),
 	))
+	m.Parser().AddOptions(parser.WithInlineParsers(
+		util.Prioritized(NewInlineMathParser(), 501),
+	))
 	m.Renderer().AddOptions(renderer.WithNodeRenderers(
 		util.Prioritized(NewMathBlockRenderer(), 501),
+		util.Prioritized(NewInlineMathRenderer(), 502),
 	))
 }
